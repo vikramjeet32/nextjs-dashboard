62cfe3ef519b3fc7f5f6fe011cec49a6624b534b
Vikramjeet Sharma <81208125+vikramjeet32@users.noreply.github.com>
2024-04-11 11:48:12 +0530

    second commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function page() {
+  return <div>This is Customers page...</div>;
+}
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function page() {
+  return <div>This is Invoices Page..</div>;
+}
+
+export default page;
